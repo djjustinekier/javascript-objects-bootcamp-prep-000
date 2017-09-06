0192b1c8df77812a9e69b78ad00662ddefab8967
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-06 17:46:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@ function updatePlaylist(obj, artistName, songTitle) {
 }
 
 function removeFromPlaylist(obj, art) {
-  delete obj.art
+  return delete obj.art
   return obj;
 }
 console.log(removeFromPlaylist(playlist, 'Paramore'))
