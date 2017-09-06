ef29fa9663bdf1f804bc1069f2e1b55c3a5dee84
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-06 17:45:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@ function updatePlaylist(obj, artistName, songTitle) {
 }
 
 function removeFromPlaylist(obj, art) {
-  function (delete obj.art) {
+  function () {delete obj.art
     return obj;
   }
 }
