2246ba9e003d4a4f8c8570cf7ac529c8efa0573f
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-06 16:26:51 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { 
+  Paramore : 'After Laughter',
+  Avicii : 'Wake me UP',
+  Kungs : 'This Girl'
+}
+
+function updatePlaylist(obj, artistName, songTitle) {
+  Object.assign({},obj, {[artistName] : songTitle});
+  return obj;
+}
